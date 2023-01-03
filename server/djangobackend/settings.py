c680e9d05e39aeb4d2f24b0b4ef9fb264e9109f8
mmiller99 <mmille60@binghamton.edu>
2023-01-03 17:18:33 -0500

    kubernetes working

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'https://mmille60-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
+
+CSRF_TRUSTED_ORIGINS = ['https://mmille60-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 # Application definition
